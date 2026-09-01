7ff4c92b31ad1f708cf1d00769b4bd712a0f839a
aashish46ak-lab <aashish46ak@gmail.com>
2026-09-01 19:27:53 +0545

    v5: PNG previews paths, Ishani splash photo path, all games active, SW v6

--- sw.js
+++ sw.js
@@ -1,52 +1,21 @@
-const CACHE = 'ishani-v3';
+const CACHE = 'ishani-v6';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './css/style.css',
-  './js/games-data.js',
-  './js/engine.js',
-  './js/app.js',
-  './js/games/memory.js',
-  './js/games/tictactoe.js',
-  './js/games/connect4.js',
-  './js/games/rps.js',
-  './js/games/dots.js',
-  './js/games/checkers.js',
-  './js/games/snake.js',
-  './js/games/pong.js',
-  './js/games/airhockey.js',
-  './js/games/battleship.js',
-  './js/games/carrom.js',
-  './js/games/ludo.js',
-  './js/games/snakesladders.js',
-  './js/games/simon.js',
-  './js/games/hangman.js',
-  './js/games/react.js',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './', './index.html', './manifest.json',
+  './css/style.css', './js/app.js', './js/engine.js', './js/games-data.js',
+  './assets/splash/ishani.png'
 ];
-
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    const copy = res.clone();
+    if (e.request.url.startsWith(self.location.origin)) {
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+    }
+    return res;
+  }).catch(() => caches.match('./index.html'))));
 });
