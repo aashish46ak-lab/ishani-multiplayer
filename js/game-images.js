b91b6648dcee7f149a9cb8144c2f719f9c74bbdb
aashish46ak-lab <aashish46ak@gmail.com>
2026-09-01 19:32:57 +0545

    merge GAME_IMAGES_A and B

--- js/game-images.js
+++ js/game-images.js
@@ -1,4 +1,1 @@
-const GAME_IMAGES = Object.assign({},
-  typeof GAME_IMAGES_A !== 'undefined' ? GAME_IMAGES_A : {},
-  typeof GAME_IMAGES_B !== 'undefined' ? GAME_IMAGES_B : {}
-);
+const GAME_IMAGES = Object.assign({}, typeof GAME_IMAGES_A!=='undefined'?GAME_IMAGES_A:{}, typeof GAME_IMAGES_B!=='undefined'?GAME_IMAGES_B:{});
